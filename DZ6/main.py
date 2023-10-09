5b380436889acf219077c359297a2458d116c063
vlborshh <vlborshh@yandex.ru>
2023-10-09 15:18:17 +0300

    DZ6

--- DZ6/main.py
+++ DZ6/main.py
@@ -12,7 +12,6 @@ def binarySearch(arr: List[int], number: int) -> int:
         else:
             right = mid - 1
     return -1
-
 arr = [1, 2, 4, 6, 10, 12]
 print(f"В массиве: {arr}")
 number = int(input("Введите элемент для поиска: "))
